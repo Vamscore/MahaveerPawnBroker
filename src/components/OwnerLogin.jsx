0aee8e347beb370505f542ab6068c6a9d63429ad
Vamscore <vamscore@gmail.com>
2026-09-21 17:40:06 +0530

    Fix owner login API connection

--- src/components/OwnerLogin.jsx
+++ src/components/OwnerLogin.jsx
@@ -1,6 +1,5 @@
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
-import { API_URL } from "../config";
 
 function OwnerLogin() {
   const navigate = useNavigate();
